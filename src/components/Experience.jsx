0805535fad9cb7a9f7996cd0b5e4acd66be93ad9
GPV <gvill005@gmail.com>
2023-11-21 22:29:27 -0800

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,45 @@
+import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
+import { Avatar } from "./Avatar.jsx";
+import { useControls } from "leva";
+export const Experience = () => {
+ 
+    const {animation} = useControls({
+      animation: {
+        value: "Typing",
+        options: ["Typing", "Falling", "Standing", "Landing"]
+      },
+    });
+
+    return(
+    <>
+      <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
+      <group position-y={-1}>
+      <ContactShadows 
+        opacity={.42} 
+        scale={10} 
+        blur={1} 
+        far={10} 
+        resolution={256} 
+        color="#000000" 
+      />
+        <Avatar animation={animation} />
+        {
+          // render chair if animation is "Typing"
+          animation === "Typing" && (
+            <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+              <boxGeometry />
+              <meshStandardMaterial color="white" />
+            </mesh>
+          )
+        }
+      
+      <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+        <planeGeometry />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      </group>
+    </>
+  );
+};
